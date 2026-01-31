35cca4d78612921b6bee6ac892362d07ae5dc6f0
OptimismDeveloper <optimismindiedev@gmail.com>
2026-01-31 20:56:32 +0100

    Update DayManager.cs

--- Assets/Scripts/DayManager.cs
+++ Assets/Scripts/DayManager.cs
@@ -1,6 +1,10 @@
+using TMPro;
 using UnityEngine;
 
 public class DayManager : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI dayText;
     [SerializeField] private int currentDay;
+
+
 }
