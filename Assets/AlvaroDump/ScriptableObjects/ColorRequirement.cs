064540497e62f8c03b13339264793afeaf6cf9d4
AlvaroVallet <alvarovalletjimenez2001@gmail.com>
2026-01-31 15:59:07 +0100

    DisplayCliente

--- Assets/AlvaroDump/ScriptableObjects/ColorRequirement.cs
+++ Assets/AlvaroDump/ScriptableObjects/ColorRequirement.cs
@@ -5,5 +5,25 @@ public enum MaskColor{
 public class ColorRequirement
 {
     public MaskColor maskColor;
-    public int porcentage;
+    public int percentage;
+
+    public string createMesageRequirement()
+    {
+        string mesagge = "";
+
+        if (percentage > 70)
+        {
+            mesagge = "Quiero que sea muy "+ maskColor;
+        } else if (percentage > 40)
+        {
+            mesagge = "Quiero que sea " + maskColor;
+        } else if (percentage > 15)
+        {
+            mesagge = "Quiero que sea un poco " + maskColor;
+        } else {
+            mesagge = "Hazla del color que quieras";
+        }
+
+        return mesagge;
+    }
 }
--- Assets/AlvaroDump/ScriptableObjects/Order.cs
+++ Assets/AlvaroDump/ScriptableObjects/Order.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Order
 {
-    public GameObject maskShape;
+    public Sprite maskShape;
     public ColorRequirement colorRequirement;
     public string RandomRequirement;
 }
