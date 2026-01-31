c181405e9c9ce1ed91550151a99ed902af9baf22
OptimismDeveloper <optimismindiedev@gmail.com>
2026-01-31 21:00:31 +0100

    Update DayManager.cs

--- Assets/Scripts/DayManager.cs
+++ Assets/Scripts/DayManager.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 public class DayManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI dayText;
-    [SerializeField] private int currentDay;
+    [SerializeField] private int currentDay = 1;
 
+    private void GoToNextDay()
+    {
+        currentDay++;
+        dayText.text = currentDay.ToString();
+    }
 
 }
